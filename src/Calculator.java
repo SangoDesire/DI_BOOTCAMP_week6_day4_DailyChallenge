079575878a7b0a49aca047f0c20e67df5a9abf9e
Sango <analysteprodez.com>
2023-02-15 09:11:11 +0000

    week6 day4 dailyChallenge

---
+++ src/Calculator.java
@@ -0,0 +1,32 @@
+class Calculator {
+    // write class fields here
+    private double num1;
+    private double num2;
+
+    public Calculator(double num1, double num2) {
+        //write definition here
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    double add() {
+        //write definition here
+        return num1 + num2;
+    }
+
+    double subtract() {
+        //write definition here
+        return num1 - num2;
+    }
+
+    double multiply() {
+        //write definition here
+        return num1 * num2;
+    }
+
+    double divide() {
+        //write definition here
+        return num2 / num1;
+    }
+
+}
